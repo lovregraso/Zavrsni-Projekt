e60498bc260f2b18fe0d6561f80023410f7a5678
lovregraso <lg52480@fer.hr>
2022-05-31 17:31:14 +0200

    comit

---
+++ configuration_file/localization/my_robot_backpack_3d.lua
@@ -0,0 +1,90 @@
+-- Copyright 2016 The Cartographer Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+include "map_builder.lua"
+include "trajectory_builder.lua"
+
+options = {
+  map_builder = MAP_BUILDER,
+  trajectory_builder = TRAJECTORY_BUILDER,
+  map_frame = "map",
+  tracking_frame = "os_imu",
+  published_frame = "base_link",
+  odom_frame = "odom",
+  provide_odom_frame = false,
+  publish_frame_projected_to_2d = false,
+  use_pose_extrapolator = true,
+  use_odometry = false,
+  use_nav_sat = false,
+  use_landmarks = false,
+  
+  use_position_sensor = false,
+  position_translation_weight = 1.,
+  nav_sat_translation_weight = 1.,
+  nav_sat_inverse_covariance_weight = 0.,
+  nav_sat_inverse_covariance_bias = 1.,
+  nav_sat_use_predefined_enu_frame = false,
+  nav_sat_predefined_enu_frame_lat_deg = 45.813902,
+  nav_sat_predefined_enu_frame_lon_deg = 16.038766,
+  nav_sat_predefined_enu_frame_alt_m = 168.259294525,
+  
+  num_laser_scans = 0,
+  num_multi_echo_laser_scans = 0,
+  num_subdivisions_per_laser_scan = 1,
+  num_point_clouds = 1,
+  lookup_transform_timeout_sec = 0.2,
+  submap_publish_period_sec = 0.3,
+  pose_publish_period_sec = 5e-3,
+  trajectory_publish_period_sec = 30e-3,
+  rangefinder_sampling_ratio = 1.,
+  odometry_sampling_ratio = 1.,
+  fixed_frame_pose_sampling_ratio = 1.,
+  imu_sampling_ratio = 1.,
+  landmarks_sampling_ratio = 1.,
+}
+
+-- tuning
+
+--TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 1 -- izmijenjeno
+--TRAJECTORY_BUILDER_3D.min_range = 0
+--TRAJECTORY_BUILDER_3D.max_range = 50
+TRAJECTORY_BUILDER_3D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.1
+--TRAJECTORY_BUILDER_3D.submaps.low_resolution = 0.8
+TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 2
+
+--pure_localization
+
+--TRAJECTORY_BUILDER.pure_localization_trimmer = {
+--    max_submaps_to_keep = 3,
+--}
+--POSE_GRAPH.optimize_every_n_nodes = 1
+MAP_BUILDER.num_background_threads = 8
+POSE_GRAPH.global_sampling_ratio = 0.003 --novo
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.3
+POSE_GRAPH.constraint_builder.min_score = 0.65
+
+
+MAP_BUILDER.use_trajectory_builder_3d = true
+--MAP_BUILDER.num_background_threads = 7 izvorno
+POSE_GRAPH.optimization_problem.huber_scale = 5e2
+--POSE_GRAPH.optimize_every_n_nodes = 320 izvorno
+--POSE_GRAPH.constraint_builder.sampling_ratio = 0.03 izvorno
+POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
+--POSE_GRAPH.constraint_builder.min_score = 0.62 izvorno
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66
+
+--TRAJECTORY_BUILDER.pure_localization = true
+
+return options
